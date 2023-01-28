14d19c992b3f9f9812cdbd0cb3c813147b7ea619
Appz-dk <patrick.fyhn@gmail.com>
2023-01-28 20:29:41 +0100

    first commit - Next.js & Chakra ui basics setup.

---
+++ src/chakra/theme.ts
@@ -0,0 +1,32 @@
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+// 1.a Import fonts used in the app
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
+
+// 2. Extend the theme to include custom colors, fonts, etc
+// Can be done seperatly like below or directly inside of 'extendTheme'
+const colors = {
+    brand: {
+        100: "FF3C00"
+    },
+};
+
+const fonts = {
+    body: "Open Sans, sans-serif"
+}
+
+const styles = {
+    global: () => ({
+        body: {
+            bg: "gray.200"
+        }
+    })
+}
+
+const components = {
+    // Button
+}
+
+export const theme = extendTheme({ colors, fonts, styles });
