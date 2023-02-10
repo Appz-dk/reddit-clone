acf9e84034c4ee2eb403ba91d27e5053310eb5ff
Appz-dk <patrick.fyhn@gmail.com>
2023-02-10 21:59:44 +0100

    Folder name change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
-import Community from "./Community/Community";
+import Community from "./DropDown/Community";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
